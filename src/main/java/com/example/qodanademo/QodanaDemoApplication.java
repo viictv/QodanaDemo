69ca3c1ca3aefdbfe227e3a5af0d95553e9d5d22
viictv <officialviic@gmail.com>
2025-02-26 10:32:36 +0100

    Initial commit

---
+++ src/main/java/com/example/qodanademo/QodanaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.qodanademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QodanaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QodanaDemoApplication.class, args);
+    }
+
+}
